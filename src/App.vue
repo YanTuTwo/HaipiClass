015a5f8fe71960c9bcd62c0f513149ae76f7b80c
zjj <454550789@qq.com>
2018-02-27 09:44:51 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app" style="height: 100%">
+    <drawer :show.sync="showUserMode" :show-mode="showModeValue" :drawer-style="{'background-color':'#fff', width: '200px'}">
+		<div slot="drawer">hahahah</div>
+		<div slot="default">
+			<m-header @showUser="showUser"></m-header>
+			<router-view></router-view>
+		</div>      
+    </drawer>  
+  </div>
+</template>
+
+<script>
+import {Popup, Drawer } from "vux";
+import MHeader from "@/components/m-header/m-header"
+export default {
+	name: "App",
+	components: {
+		MHeader,
+    	Popup,
+   		Drawer
+  	},
+  	data() {
+    	return {
+   			showUserMode: false,
+      		showModeValue: "push"
+    	};
+  	},
+	methods:{
+		showUser(){
+			this.showUserMode=!this.showUserMode;
+		}
+	}
+};
+</script>
+
+<style>
+@import "./assets/styles/common.css";
+body {
+  	background-color: #fbf9fe;
+}
+html,body {
+  	height: 100%;
+  	width: 100%;
+ 	overflow-x: hidden;
+}
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,36 @@
+<template>
+	<x-header>
+		<span slot="overwrite-left">
+			<x-icon type="navicon" size="35" style="fill:#fff;position:relative;top:-8px;left:-3px" @click="showUser"></x-icon>				
+		</span>
+		<div class="logo"><img src="../../assets/logo.png" alt=""></div>
+	</x-header>
+</template>
+<script>
+import {XHeader} from "vux"
+export default {
+	components:{
+		XHeader
+	},
+	methods:{
+		showUser(){
+			this.$emit('showUser');
+		}
+	}
+}
+</script>
+<style lang="scss">
+	.logo{
+		width: 100%;
+		height: 40px;
+		position: relative;
+		text-align: center;vertical-align: middle;
+		img{
+			width: 60%;
+			transform:translate(-50%, -50%);
+			position: absolute;
+			top: 50%;
+			left: 50%;
+		}
+	}
+</style>
